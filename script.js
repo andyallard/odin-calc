9aea81075f3af1dac28947261de61bc717213f9d
Andy Allard <109981829+andyallard@users.noreply.github.com>
2024-02-26 03:45:17 -0800

    Clear, sign change, and number buttons now work

--- script.js
+++ script.js
@@ -39,28 +39,72 @@ function setupQuerySelectors() {
     })
 }
 
-function handleButtonPress(event) {
+function debounce(func, delay) {
+    let timeoutId;
+    return function() {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func.apply(this, arguments);
+      }, delay);
+    };
+  }
+  
+// const handleClick = debounce(function() {
+// // Your click event handling code
+// }, 300); // Set an appropriate delay in milliseconds
+
+const handleButtonPress = debounce(function(event) {
     classes = Array.from(event.target.classList);
     console.log(classes);
+    keystroke = event.target.textContent;
+    console.log(`${keystroke} button pressed`);
 
     switch (true) {
         case classes.includes('number'):
-
+            handleNumberPress(keystroke);
             break;
         case classes.includes('operator'):
 
             break;
+        case classes.includes('clear'):
+            clearScreen();
+            break;
+        case classes.includes('equals'):
+
+            break;
+        case classes.includes('sign-change'):
+            handleSignChange();
+            break;
     }
+}, 100);
 
-    keystroke = event.target.textContent;
-    console.log(`${keystroke} button pressed`);
-
+function handleSignChange() {
+    DISPLAY.textContent = parseFloat(DISPLAY.textContent) * -1;
 }
 
-let a = b = operator = null;
+function handleNumberPress(keystroke) {
+    // can't have multiple decimal places
+    if (keystroke == '.' && DISPLAY.textContent.includes('.')) { return; }
+    
+    // display only has so much space
+    if (DISPLAY.textContent.length >= 12) { return; }
 
-let display = document.querySelector('.display');
-display.textContent = 0;
+    if (DISPLAY.textContent == '0' && keystroke != '.') {
+        DISPLAY.textContent = keystroke;
+    } else {
+        DISPLAY.textContent += keystroke;
+    }
+}
+
+function clearScreen() {
+    DISPLAY.textContent = '0';
+    A = B = OPERATOR = null;
+}
+
+let A = B = OPERATOR = null;
+
+let DISPLAY = document.querySelector('.display');
+DISPLAY.textContent = 0;
 
 setupQuerySelectors();
 
